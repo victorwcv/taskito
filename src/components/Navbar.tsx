b390f06b4355b6217fd1b8f31e0e695e580a791b
William <victorwccv@gmail.com>
2025-06-04 23:02:41 -0500

    chore: update theme in tailwindcss v4 from tailwind.config.js

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar: React.FC = () => {
     <header className="h-16 bg-zinc-900 text-white">
       <div className="container mx-auto px-4 h-full flex justify-between items-center">
         <a href="https://victorwcv.github.io/taskito/" className="flex items-center gap-2">
-          <h1 className="text-2xl font-bold">Taskito -  <span className="text-cyan-400">Kanban Board</span></h1>
+          <h1 className="text-2xl font-bold">Taskito -  <span className="text-accent-500">Kanban Board</span></h1>
         </a> 
 
         <nav className="block">
